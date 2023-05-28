ff2f1fd87e3648f82dbc177d037f29f1cd1058aa
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-05-28 22:01:13 +0200

    Update Main.java

--- Java Warehouse - No DB/Main.java
+++ Java Warehouse - No DB/Main.java
@@ -1,5 +1,4 @@
-package eTivityWarehouses;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	
@@ -11,74 +10,96 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// 1st Version with no DataBase involved for the example
-					
-		Product p1 = new Product(1,20.4,"Headphones",200,1);
-		Product p2 = new Product(2, 289.5, "Apple Watch",4,1);
+		Shop shop = new Shop();
 		
-		Warehouse w1 = new Warehouse(1,"55th avenue, New York");
+		Warehouse w1 = shop.createWarehouse(1,"55th avenue, New York", "Online");
 		
-		User u1 = new User(420, "Francesco", "Tarantino", "Hell, Norway","User");
+		Product p1 = shop.createProduct("Apple Watch SE", 299.99, 4, 1);
+		Product p2 = shop.createProduct("Apple iPhone 8", 199.99, 12, 1);
+		Product p3 = shop.createProduct("iPhone 14", 899.99, 3, 2);
 		
-		Order o1 = new Order(1, u1, p1);
+		User u1 = shop.createUser("Francesco", "Tarantino", "Hell, Norway");
 		
-		Order o2 = new Order(2, u1, p2);
+		Order o1 = shop.createOrder(u1);
 		
+		o1.addProduct(p1);
+		o1.addProduct(p2);
 		
-		
-		
-		int total1 = p1.getQuantity();
-		int total2 = p2.getQuantity();
-		System.out.println("^** This software is created for testing and educational purposes only. **^");
-
-		
+		double price1 = 0;
+		int quantity;
 		
 		Scanner in = new Scanner(System.in);
 		int choice = 0;
 		
 		while(choice!=3) {
-			System.out.println("");
-			System.out.println("Welcome, "+PURPLE+u1.getUsername()+RESET+" - "+u1.role());
-			System.out.println("");
-			System.out.println("---------------");
-			System.out.println(YELLOW+"-- Products  "+RESET);
-			System.out.println("---------------");
-			System.out.println(" ## "+p1.getDesc()+" ## ");
-			System.out.println("Price: "+p1.getPrice());
-			System.out.println("Quantity: "+p1.getQuantity());
-			System.out.println("Wharehous ID: "+p1.getIdWarehouse());
-			System.out.println("");
-			System.out.println(" ## "+p2.getDesc()+" ## ");
-			System.out.println("Price: "+p2.getPrice());
-			System.out.println("Quantity: "+p2.getQuantity());
-			System.out.println("Wharehouse ID: "+p2.getIdWarehouse());
-			System.out.println("");
-			System.out.println("");
-			System.out.println("Select one option available");
-			System.out.println("1. "+p1.getDesc());
-			System.out.println("2. "+p2.getDesc());
-			System.out.println("3. Exit ");
 			
+			System.out.println("^** This software is created for testing and educational purposes only. **^");
+			
+			System.out.println("");
+			System.out.println("Welcome, "+PURPLE+u1.getName()+RESET);
+			System.out.println("");
+			
+			System.out.println("Products in the Shop:");
+			for(Product product : shop.getProducts()) {
+				System.out.println(product.getId()+": "+product.getname()+" - "+GREEN+"$"+product.getPrice()+RESET);
+				System.out.println("Only "+product.getQuantity()+" left");
+			}
+			
+			System.out.println("");
+			System.out.println("** MENU **");
+			System.out.println("Select an option (1, 2, 3)");
+			System.out.println("1. Process order");
+			System.out.println("2. Admin view");
+			System.out.println("3. Terminate");
 			choice = in.nextInt();
 			
 			switch(choice) {
 			case 1:
-				o1.processOrder();
+				System.out.println("");
+				System.out.println(GREEN+"Thank you for your order! "+RESET);
+				System.out.println("");
+				System.out.println("");
+				System.out.println("\nOrder Details:");
+				System.out.println("");
+				for(Order order : shop.getOrders()) {
+					System.out.println("Order ID: "+order.getId());
+					System.out.println("User: "+order.getUser());
+					System.out.println("Products: ");
+					System.out.println("");
+					for(Product product : order.getProducts()) {
+						System.out.println("- "+product.getname());
+						System.out.println("$"+product.getPrice());
+						
+						price1 = price1 + product.getPrice();
+						
+						quantity = product.getQuantity() - 1;
+						
+						System.out.println("Now only "+quantity+" left");
+					}
+					
+					double total = price1;
+					System.out.println("");
+					System.out.println("Total:");
+					System.out.println(+total+"$");
+					System.out.println("");
+				}
+				
 				break;
 			case 2:
-				o2.processOrder();
+				System.out.println();
 				break;
 			
 		
 			}
+			
 		}
-
-
+		
+		
+		
+		
+		
+			
+		}
 	}
-	
-	public static int totalProduct(int quantity1, int quantity2) {
-		return quantity1 + quantity2;
-	}
-	
 
-}
+
