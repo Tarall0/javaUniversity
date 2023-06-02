49f91fa93af38b59ae3898dbbb427fdbb585f1d3
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-06-02 20:38:33 +0200

    DB

---
+++ Java Warehouse - DB/Order.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Order{
+	
+	private int id;
+	private User user;
+	private List<Product> products;
+	
+	public Order(int id, User user) {
+		this.id = id;
+		this.user = user;
+		this.products = new ArrayList<>();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getUser() {
+		return user.getName();
+	}
+	
+	public void addProduct(Product product) {
+		products.add(product);
+	}
+	
+	public List<Product> getProducts(){
+		return products;
+	}
+	
+
+}
