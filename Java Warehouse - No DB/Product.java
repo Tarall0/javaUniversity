91cc0a72686fea94630025004c2362b8d3c2966e
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-06-03 23:39:30 +0200

    Update Product.java
    Adding methods to save delete and edit product

--- Java Warehouse - No DB/Product.java
+++ Java Warehouse - No DB/Product.java
@@ -1,18 +1,57 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class Product extends Warehouse {
+public class Product  {
 	
 	private int id;
 	private String name;
 	private double price;
 	private int quantity;
 	
-	public Product(int id, String name, double price, int quantity, int id_warehouse) {
-		super(id_warehouse);
+	public Product(int id) {
+		this.id = id;
+	}
+	
+	public Product(int id, String name, double price, int quantity) {
+		
 		this.id = id;
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
 	}
+	
+	public void save(Connection connection) throws SQLException {
+        String query = "INSERT INTO products (id, name, price, quantity) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            statement.setString(2, name);
+            statement.setDouble(3, price);
+            statement.setInt(4, quantity);
+            statement.executeUpdate();
+        }
+    }
+	
+	public void del(Connection connection) throws SQLException {
+		String query = "DELETE FROM products WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        }
+    }
+	
+	public void upd(Connection connection, int productId, String editName, double newPrice, int editQuantity) throws SQLException {
+		String query = "UPDATE products SET name=?, price=?, quantity=? WHERE id=?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+        	statement.setString(1, editName);
+            statement.setDouble(2, newPrice);
+            statement.setInt(3, editQuantity);
+            statement.setInt(4, productId);
+            statement.executeUpdate();
+        }
+    }
+
+	// Methods to get values from Product
 	
 	public int getId() {
 		return id;
@@ -30,6 +69,23 @@ public class Product extends Warehouse {
 		return quantity;
 	}
 	
+	// Methods to set new values Product
+	
+	public void setPrice(double edit_price) {
+		this.price = edit_price;
+	}
+	
+	public void setName(String edit_name) {
+		this.name = edit_name;
+	}
+	
+	public void setQuantity(int edit_quantity) {
+		this.quantity = edit_quantity;
+	}
+	
+	public void takeOne() {
+		this.quantity = quantity - 1;
+	}
 	
 
 }
