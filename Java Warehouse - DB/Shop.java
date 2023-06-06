fe149facc31d9ea1f0b7a60ad7bfc0d60546e868
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-06-06 11:47:09 +0200

    Update Shop.java

--- Java Warehouse - DB/Shop.java
+++ Java Warehouse - DB/Shop.java
@@ -6,14 +6,14 @@ import java.sql.*;
 public class Shop {
 	
 	// Database connection properties
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/shopuni";
+    private static final String DB_HOST = "jdbc:mysql://localhost:3306/shopuni";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "";
 
     // Database table names
     private static final String TABLE_PRODUCTS = "products";
     private static final String TABLE_USERS = "user";
-    private static final String TABLE_ORDERS = "orders";
+    
     
 
     
@@ -27,7 +27,7 @@ public class Shop {
     }
 
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        return DriverManager.getConnection(DB_HOST, DB_USERNAME, DB_PASSWORD);
     }
     
     public List<Product> getAllProducts(Connection connection) throws SQLException {
@@ -71,27 +71,7 @@ public class Shop {
         return product;
     }
     
-    public List<Product> getProductsForOrder(Connection connection, int orderId) {
-        List<Product> products = new ArrayList<>();
-        String query = "SELECT * FROM products p JOIN order_products op ON p.id = op.product_id WHERE op.order_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, orderId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String name = resultSet.getString("name");
-                    double price = resultSet.getDouble("price");
-                  
-                    Product product = new Product(id, name, price);
-                    products.add(product);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return products;
-    }
-    
+  
 
     public List<User> getAllUsers(Connection connection) throws SQLException {
         List<User> users = new ArrayList<>();
@@ -111,27 +91,6 @@ public class Shop {
         return users;
     }
     
-    
-    public User getUserAdmin(Connection connection, int userId) throws SQLException {
-        String query = "SELECT * FROM " + TABLE_USERS + " WHERE id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, userId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String name = resultSet.getString("name");
-                    String lastname = resultSet.getString("lastname");
-                    String address = resultSet.getString("address");
-                    String role = resultSet.getString("role");
-                    return new User(id, name, lastname, address, role);
-                   
-                    
-                }
-            }
-        }
-        return null;
-    }
-
-
+   
 
 }
